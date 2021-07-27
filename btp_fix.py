84a09301fedcde02b67b0b28189985a072b55ca1
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-07-27 08:55:32 +0400

    changed any comments

--- btp_fix.py
+++ btp_fix.py
@@ -3,7 +3,7 @@ fin = open("config.txt", "rt")
 fout = open("C:\ebyn\BTP.bat", "wt")
 # For each line in the input file
 for line in fin:
-	#read replace the string and write to output file
+	# Read replace the string and write to output file
 	fout.write(line.replace('C:\\j2sdk1.4.2_08\\bin\\', ''))
 # Close input and output files
 fin.close()
